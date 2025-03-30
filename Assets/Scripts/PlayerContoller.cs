9b92a4e02b0e5839ad9644c0b80f2df5c8e2215d
smk_laptop <smk@hp_laptop.ru>
2025-03-30 15:49:38 +0300

    add animation

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -14,8 +14,11 @@ public class PlayerContoller : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
 
+    private Animator anim;
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,16 +26,49 @@ public class PlayerContoller : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+
+
+
+
+
+
+
+
     }
 
-
-    public void Update()
+    private void Update()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = Vector2.up * jumpForce;
+            anim.SetTrigger("takeOF");
         }
+        if(isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            anim.SetBool("isJumping", true);
+        }
+
+
+
+
+
+
+
     }
 }
